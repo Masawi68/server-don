3b49dd9af5b0f8f0493321221cb23688d37ef005
Klue <edmasawi@ymail.com>
2024-01-07 21:18:31 +0100

    forth commit

--- app.js
+++ app.js
@@ -12,18 +12,6 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-// PostgreSQL database configuration
- /* const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
-  ssl: {
-    rejectUnauthorized: false,
-  },
-});  */
-
  const connectionString = process.env.DB_URL;
 
 // Create a connection pool
